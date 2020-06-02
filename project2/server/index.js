50ef99286498eb8606ee3c8e003c19434bb24c01
Cezary Krawczyk <ckrawczyk@atlassian.com>
2020-06-02 22:51:33 +0200

    add routing

--- project2/server/index.js
+++ project2/server/index.js
@@ -1,11 +1,19 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("./mongoose");
+const userController = require("./controller/UserController");
 
-var app = express();
+const app = express();
+app.use(bodyParser());
+app.use(cors());
+
+app.use("/api", userController);
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
-app.listen(5000, function () {
-    console.log("Example app listening on port 3000!");
+app.listen(5000, () => {
+    console.log("Example app listening on port 5000!");
 });
